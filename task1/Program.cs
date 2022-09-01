43c43b3edc29e0046a9c484bf1f558d753f940cd
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-01 20:15:14 +0300

    нули и единицы

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами и вывести их на экран
+void FillArray(int [] eightnumbers)
+{
+    int i = 0;
+    while(i < eightnumbers.Length)
+    {
+        eightnumbers[i] = new Random().Next(0, 2);
+        i++;
+    }
+}
+void PrintArray(int [] eightnumbers)
+{
+    int i = 0;
+    while (i<eightnumbers.Length)
+    {
+      Console.Write(eightnumbers[i] );
+      i++;  
+    }
+}
+  int [] array = new int[8];
+  FillArray(array);
+  PrintArray(array);
