1a7b2a575e7297e9f61f784e5af7f3acd2b2ec9e
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-01 22:06:30 +0300

    Решение не доконца

---
+++ task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задать массив из 12 элементов, заполненных числами из  [0,9].Найти сумму положительных элементов массива
+void FillArray(int [] twelvenumbers)
+{
+    int i = 0;
+    while(i < twelvenumbers.Length)
+    {
+        twelvenumbers[i] = new Random().Next(0, 10);
+        i++;
+    }
+}
+void PrintArray(int [] twelvenumbers)
+{
+    int i = 0;
+    while (i<twelvenumbers.Length)
+    {
+      Console.Write(twelvenumbers[i] + " ");
+      i++;  
+    }
+}
+  int [] array = new int[12];
+  
+  FillArray(array);
+  PrintArray(array);
+  int size = 12
+int sum  = 0;
+int i = 0;
+while (i<12)
+{
+      array[i]= sum;
+       sum = sum + array[i];
+       i++;
+      }
+  Console.WriteLine(sum);
+   
+ 
