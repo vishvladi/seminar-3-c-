37adc83eb5463eab2fd1e926abf0a409dabd52d2
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-03 08:30:26 +0300

    начало есть

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Написать программу замены элементов массива на противоположные
+Console.Write ( "Enter size of array: ");
+int size = int.Parse(Console.ReadLine()??"0");
+int []array = new int  [size];
+int i = 0;
+while(i<array.Length)
+{
+    array[i]= new Random().Next(1, 10);
+    i++;
+}
+i=0;
+while(i<array.Length)
+{
+  Console.Write($"{array[i]}"+" ");
+  i++;  
+}
+ 
+
+
