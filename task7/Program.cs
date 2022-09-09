1a7ca6e227b07cca101c82838ad75e764f549843
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-10 01:09:54 +0300

     Cумма чисел на нечетной позиции

--- task7/Program.cs
+++ task7/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Найти сумму всех чисел стоящих на нечетной позиции
-Console.WriteLine("Hello, World!");
+int [] array = new int [10];
+ int i = 0;
+ int sum = 0;
+  
+ while (i<array.Length)
+ {
+ array [i] = new Random().Next(1, 100);
+     if (i !=0)
+     sum = sum + array[i];
+
+i++;
+ }
+ Console.Write(" Сумма чисел на нечетной позиции: " + sum);
