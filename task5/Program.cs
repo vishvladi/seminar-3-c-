58b99034cdbcaf750921d7ea29f7a1bca49a374f
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-10 00:34:27 +0300

    how many three digit numbers

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,2 +1,17 @@
-﻿// Задать массив, заполнить случайными положительными трехзначными числами, показать количество четных чисел
-Console.WriteLine("Hello, World!");
+﻿// Задать массив, заполнить случайными положительными трехзначными числами, 
+//показать количество четных чисел
+int [] array = new int [10];
+ int i = 0;
+ int count = 0;
+  
+ while (i<array.Length)
+ {
+ array [i] = new Random().Next(100, 1000);
+ Console.Write(array[i]+" ");
+     if (array[i]%2==0)
+     count++;
+
+i++;
+ }
+ Console.Write("Четных чисел: " + count);
+ 
