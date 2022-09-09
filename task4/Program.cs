1424fa91ca0de7572a90673a3b04a7238914f2c8
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-10 00:14:40 +0300

    есть ли в массиве заданное число

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,24 @@
-﻿// Определить присутствует в заданном массиве целое число
+﻿// Определить присутствует ли в заданном массиве  некоторое число
+Console.WriteLine("Введите число: ");
+ int [] array = new int [10];
+ int i = 0;
+ int n = int.Parse(Console.ReadLine()??"0");
+ while (i<array.Length)
+ {
+ array [i] = new Random().Next(1, 100);
+ Console.Write(array[i]+" ");
  
+     if (array [i]==n)
+     {
+       Console.WriteLine("Это число есть в массиве");
+     }
+     
+i++; 
+}
+
+Console.WriteLine("Этого числа нет в массиве");
+ 
+
+
+
+
