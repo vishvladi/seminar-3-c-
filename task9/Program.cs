44906da60463ea9701183e8628ef298493be630c
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-12 20:47:32 +0300

    difference between max and min

--- task9/Program.cs
+++ task9/Program.cs
@@ -1,2 +1,27 @@
-﻿// В указанном массиве вещественных чисел найдите разницу мужду максимальным и минимальным
-Console.WriteLine("Hello, World!");
+﻿// В указанном массиве вещественных чисел найдите разницу мужду максимальным и минимальным элементом
+
+ double[] array = new double[10];
+  for (int i = 0; i < array.Length; i++ )
+  {
+    array [i] = new Random().Next(1, 10);
+    Console.Write(array[i] + " ");
+  }
+
+double max = array [0];
+double min = array [0];
+
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (max  < array [i])
+    {
+      max  = array [i];
+    }
+     if (min  > array [i])
+    {
+      min  = array [i];
+    }
+  }
+
+  double difference = max - min;
+
+  Console.WriteLine($"\nразница между между максимальным ({max}) и минимальным({min}) элементами: {difference}");
