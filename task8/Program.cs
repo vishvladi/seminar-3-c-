bbda4d4a9c1843658fc8d5fc59b1b1fe1412b7c5
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-12 19:58:38 +0300

    произведение пар чисел

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,19 +1,11 @@
 ﻿//Найти произведение пар чисел в одномерном массиве. Парой считается первый и последний,
 // второй и предпоследний и т.д
-int [] array = new int [10];
- int i = 0;
- 
- int a = 0;
- int b = 0;
+int [] array = {3, 2, 3, 5, 4 ,6, 1, 8 , 9, 8};
+  int result = 0;
+  for (int i = 0; i < array.Length/2; i++){
+     result = array [i] * array[array.Length - 1 - i];
+     Console.WriteLine ("Произведение противоположных элементов = " + result);
+  }
   
- while (i<array.Length)
- {
- array [i] = new Random().Next(1, 10);
-      while (i)
-    
- Console.Write(a*b);
-    
-
-i++;
- }
  
+   
